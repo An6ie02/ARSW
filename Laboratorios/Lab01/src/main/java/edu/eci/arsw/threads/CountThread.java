a013e4223933878bcc30779d1b9391897309fdcd
ELS4NTA <daniel1sa_@hotmail.com>
2023-08-22 21:32:24 -0500

    Parte I - Introducción a Hilos

--- Laboratorios/Lab01/src/main/java/edu/eci/arsw/threads/CountThread.java
+++ Laboratorios/Lab01/src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -8,7 +8,24 @@ package edu.eci.arsw.threads;
 /**
  *
  * @author hcadavid
+ * @author Angie Mojica
+ * @author Daniel Santanilla
  */
-public class CountThread {
+public class CountThread extends Thread {
+
+    private int A;
+    private int B;
+
+    public CountThread(int A, int B) {
+        this.A = A;
+        this.B = B;
+    }
+
+    @Override
+    public void run() {
+        for (int i = A; i <= B; i++) {
+            System.out.println(i);
+        }
+    }
     
 }
